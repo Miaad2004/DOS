44d2dc9f161ca8bb72b310c25ed35e9cb89428dd
Miaad <Miaad@MIAAD>
2024-12-26 18:40:49 +0330

    initial commit

---
+++ src/file_system.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int test() {
+    printf("Hello, from FS!\n");
+    return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+# include "file_system.h"
+
+int main() {
+    printf("Hello, World!\n");
+    test();
+    return 0;
+}
